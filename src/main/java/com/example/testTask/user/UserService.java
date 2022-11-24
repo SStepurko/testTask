7a1d821945a09fcb8c636a358e705e5befae31a0
Sergey Stepurko <82845341+SStepurko@users.noreply.github.com>
2022-11-24 16:17:48 +0500

    Data layer

--- src/main/java/com/example/testTask/user/UserService.java
+++ src/main/java/com/example/testTask/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.testTask.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -7,10 +8,19 @@ import java.util.List;
 // Service layer
 @Component
 public class UserService {
+
+	private final UserRepository userRepository;
+
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	public List<AppUser> getUsers() {
-		return List.of(
-				new AppUser(1L, "user1", "pass1", "msg1"),
-				new AppUser(2L, "user2", "pass2", "msg2")
-		);
+		return userRepository.findAll();
+//		return List.of(
+//				new AppUser(1L, "user1", "pass1", "msg1"),
+//				new AppUser(2L, "user2", "pass2", "msg2")
+//		);
 	}
 }
